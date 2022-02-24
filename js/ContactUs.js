dddd5bbe66f8a2c4f22c48c4a36a8bebf2bb736a
duyduoc03 <duyduoc03bg@gmail.com>
2022-02-24 09:36:53 +0700

    new

---
+++ js/ContactUs.js
@@ -0,0 +1,57 @@
+function check()
+{
+	ten = document.getElementById("name");
+	errname = document.getElementById("loiname");
+    email = document.getElementById("email");
+	errEmail = document.getElementById("loiEmail");
+    phone = document.getElementById("phone");
+    errPhone = document.getElementById("loiPhone");
+    //name
+    if(ten.value=="")
+    {
+        errname.innerHTML="This field is required";
+        errname.style.color="red";
+        return false;
+    }
+    else
+        errname.innerHTML="";
+    //mail
+    if(email.value=="")
+    {
+        errEmail.innerHTML="Please enter your email address";
+        errEmail.style.color="red";
+        return false;
+    }
+    else
+        errEmail.innerHTML="";
+
+    regEmail = /^\w+@\w+(\.\w+)+$/; 
+    if(regEmail.test(email.value)==false)
+    {
+        errEmail.innerHTML="Email must be in format: abc@domain.com";
+        errEmail.style.color="red";
+        return false;
+    }
+    else
+        errEmail.innerHTML="";
+    //phone 
+    if(phone.value=="")
+    {
+        errPhone.innerHTML="Please enter your phone number";
+        errPhone.style.color="red";
+        return false;
+    }
+    else
+        errPhone.innerHTML="";
+
+    regPhone = /^\d{10}$/;
+    if(regPhone.test(phone.value)==false)
+    {
+        errPhone.innerHTML="Please enter valid phone number";
+        errPhone.style.color="red";
+        return false;
+    }
+    else
+        errPhone.innerHTML="";
+    return true;
+}
